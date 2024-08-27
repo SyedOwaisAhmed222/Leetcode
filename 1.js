6963b1ec8d8e70e55b460c41298a848d5ece298b
Syed Owais Ahmed <syed.owais@koderlabs.com>
2024-08-27 14:25:17 +0500

    added brute force solution

--- 1.js
+++ 1.js
@@ -4,14 +4,28 @@ var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     map[nums[i]] = i;
   }
-  console.log(map);
   for (let j = 0; j < nums.length; j++) {
-    if (( map[target - nums[j]] ) && map[target - nums[j]]!==j) {
+    if (map[target - nums[j]] && map[target - nums[j]] !== j) {
       return [j, map[target - nums[j]]];
     }
   }
   return [];
 };
-numbers = [1,3,4,2];
+
+// var twoSum = function (nums, target) {
+//   res = []
+//   for (let i = 0; i < nums.length - 1; i++) {
+//       for (let j = i + 1; j < nums.length; j++) {
+//           if (nums[i] + nums[j] === target) {
+//               res = [i, j];
+//               return res
+//           }
+//       }
+
+//   }
+//   return res
+// };
+
+numbers = [1, 3, 4, 2];
 const result = twoSum(numbers, 6);
 console.log(result);
